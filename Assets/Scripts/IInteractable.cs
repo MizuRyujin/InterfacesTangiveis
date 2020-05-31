e9eb3e2186bf9faab42eb9371a4497bb7ae2e843
JBernardoRebelo <bernasfasttaker@gmail.com>
2020-05-31 18:50:16 +0100

    Add wumpa code

--- Assets/Scripts/IInteractable.cs
+++ Assets/Scripts/IInteractable.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using UnityEngine;
-
+﻿/// <summary>
+/// Interactable interface
+/// </summary>
 public interface IInteractable
 {
-    Action OnInteract();
     void Interact();
 }
--- Assets/Scripts/Wumpa.cs
+++ Assets/Scripts/Wumpa.cs
@@ -1,19 +1,66 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class Wumpa : MonoBehaviour, IInteractable
 {
-    public void Interact()
+    [SerializeField] private float respawnTime = 5f;
+
+    private MeshRenderer _mesh;
+    private CapsuleCollider _collider;
+    private ParticleSystem _particles;
+    private WaitForSeconds _respawnTimer;
+
+    private void Awake()
     {
-        throw new NotImplementedException();
+        _mesh = gameObject.GetComponent<MeshRenderer>();
+        _collider = gameObject.GetComponent<CapsuleCollider>();
+        _particles = gameObject.GetComponentInChildren<ParticleSystem>();
+        _respawnTimer = new WaitForSeconds(respawnTime);
     }
 
-    public Action OnInteract()
+    public void Interact()
     {
-        throw new NotImplementedException();
+        Debug.Log("Interacting");
+
+        Despawn();
     }
 
     public void OnTriggerEnter()
     {
+        Interact();
+    }
+
+    /// <summary>
+    /// Hides fruit
+    /// </summary>
+    private void Despawn()
+    {
+        _collider.enabled = false;
+        _mesh.enabled = false;
+        _particles.gameObject.SetActive(false);
+
+        StartCoroutine(TimerRespawn());
+    }
+
+    /// <summary>
+    /// Respawns fruit
+    /// </summary>
+    private void Respawn()
+    {
+        _collider.enabled = true;
+        _mesh.enabled = true;
+        _particles.gameObject.SetActive(true);
+    }
+
+    /// <summary>
+    /// Courotine Timer
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator TimerRespawn()
+    {
+        yield return _respawnTimer;
+
+        Respawn();
     }
 }
