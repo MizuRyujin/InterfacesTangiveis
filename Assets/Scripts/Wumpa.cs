ecffe5d0792a5a96bab6aa23759cef985bc0dc07
JBernardoRebelo <bernasfasttaker@gmail.com>
2020-05-31 18:10:01 +0100

    Merge and add wumpas

--- Assets/Scripts/Wumpa.cs
+++ Assets/Scripts/Wumpa.cs
@@ -1,7 +1,18 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Wumpa : MonoBehaviour, IInteractable
 {
+    public void Interact()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Action OnInteract()
+    {
+        throw new NotImplementedException();
+    }
+
     public void OnTriggerEnter()
     {
     }
